326cf3c8aa0051504f7e1457c3cf0e6520af9f99
jyotendra02 <jyotendrachandan02@gmail.com>
2024-02-21 12:00:00 +0530

    React Vite Installed along with simple and svg maps

---
+++ Maps/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import IndiaMap from './components/IndiaMap'
+
+function App() {
+  
+
+  return (
+    <>
+      <IndiaMap></IndiaMap>
+    </>
+  )
+}
+
+export default App
