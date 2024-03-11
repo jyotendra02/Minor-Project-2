a3baa92ddec0b3316cf115272f1dd125530c981c
Prabal Tripathi <prabal2535@gmail.com>
2024-03-11 12:37:16 +0530

    india map resolved

--- Maps/src/components/IndiaMap.jsx
+++ Maps/src/components/IndiaMap.jsx
@@ -2,56 +2,6 @@ import React, { useState } from "react";
 import India from "@svg-maps/india";
 import { SVGMap } from "react-svg-map";
 import "react-svg-map/lib/index.css";
-<<<<<<< HEAD
-import ReactDatamaps from "react-india-states-map";
-import data from "./data.json";
-
-function App() {
-  const [stateData, setStateData] = useState({
-    code: "",
-    name: "",
-    value: "",
-    content: "",
-    area:""
-  });
-
-  function onLocationClick(event) {
-    const clickedState = event.target.getAttribute("name");
-    const newStateData = { ...stateData };
-
-    newStateData.code = event.target.id;
-    newStateData.name = clickedState;
-
-    // Access the data for the clicked state from the JSON file
-    if (data[clickedState]) {
-      newStateData.value = data[clickedState].value;
-      newStateData.area= data[clickedState].content.area;
-      newStateData.content = data[clickedState].content.txt;
-    } else {
-      // Reset the values if the clicked state is not in the JSON file
-      newStateData.value = "";
-      newStateData.content = "";
-      newStateData.area = "";
-    }
-
-    setStateData(newStateData);
-  }
-
-  return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <div className="m-4 flex-1">
-          <p>Value: {stateData.value}</p>
-          <p>Area: {stateData.area}</p>
-          <p>Content: {stateData.content}</p>
-          <p>State Name: {stateData.name}</p>
-          <p>State Code: {stateData.code}</p>
-        </div>
-
-        <div className="map-container">
-          <SVGMap map={India} onLocationClick={onLocationClick} />
-        </div>
-=======
 import data from "./data.json";
 import StateDetails from "./StateDetails";
 
@@ -80,13 +30,8 @@ function App() {
       <div className="flex-1 bg-white">
         {/* Render StateDetails component if a state is selected */}
         {selectedState && <StateDetails stateData={selectedState} />}
->>>>>>> ae45d3d23705700e272ac2d449835b23eecad49c
       </div>
     </div>
   );
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> ae45d3d23705700e272ac2d449835b23eecad49c
 export default App;
